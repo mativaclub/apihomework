0a10d7ba5bddc6f700562e781bce1ed28e870a79
mativaclub <96348077+mativaclub@users.noreply.github.com>
2022-03-09 22:53:06 +0200

    Initial commit

---
+++ src/main/java/com/apihomework/apihomework/ApihomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.apihomework.apihomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApihomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApihomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apihomework/apihomework/ApihomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apihomework.apihomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApihomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
